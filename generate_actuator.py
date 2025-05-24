ed69ae6195ce21adaf265dd6e60658c5ef6e87aa
ocanath <ocanath@gmail.com>
2025-05-24 07:47:45 -0700

    stator housing basic shape added

--- generate_actuator.py
+++ generate_actuator.py
@@ -20,8 +20,8 @@ class FramelessMotor:
 		self.StatorID = 30
 		self.IsInrunner = True
 		if(self.IsInrunner):
-			self.StatorODMountClearance = 0.5	#radial dimension
-		self.WireClearance = 1	#mm, applied to top and bottom
+			self.StatorODMountClearance = 3	#radial dimension
+		self.WireClearance = 2	#mm, applied to top and bottom
 		
 		self.RotorOD = 28
 		self.RotorID = self.RotorOD/2
@@ -36,7 +36,7 @@ class MotorParams:
 			self.bottomBearing = Bearing(16, 8, 4)
 		else:
 			self.rotorBearing = Bearing(10,5,5)	#bleh
-		self.BaseThickness = 2	#thickness of the base
+		self.BottomBearingSupportThickness = 2	#thickness of the base
 		self.OuterWallThickness = 2
 		self.GearboxHeightClearance = 2
 #some motor parameter class, pulled all design defining dims from the yaml
@@ -54,13 +54,19 @@ if(mp.stator.IsInrunner == True):
 	parr = []
 	
 	p1 = App.Vector(mp.bottomBearing.OD - mp.bottomBearing.OuterRaceClearance,0)
-	p2 = App.Vector(p1.x,mp.BaseThickness)
+	p2 = App.Vector(p1.x,mp.BottomBearingSupportThickness)
 	innerWallRadius = mp.stator.StatorOD	#line to line
 	p3 = App.Vector(innerWallRadius-mp.stator.StatorODMountClearance, p2.y)
-	p4 = App.Vector(p3.x, p3.y + mp.stator.WireClearance)
-	p5 = App.Vector(innerWallRadius, p4.y)
+	p4 = App.Vector(p3.x, p3.y + mp.stator.WireClearance) #vertical
+	p5 = App.Vector(innerWallRadius, p4.y) #horizontal
 	heightFromStatorBase  = mp.stator.StatorHeight + mp.topBearing.Height + mp.GearboxHeightClearance
-	p6 = App.Vector(p5.x, p5.y + heightFromStatorBase)	#increase y to add clearance for the top bearing
+	p6 = App.Vector(p5.x, p5.y + heightFromStatorBase) #vertical	#increase y to add clearance for the top bearing
+	outerWallRadius = innerWallRadius + mp.OuterWallThickness
+	p7 = App.Vector(outerWallRadius, p6.y)	#horizontal
+	p8 = App.Vector(p7.x, -mp.bottomBearing.Height)
+	p9 = App.Vector(mp.bottomBearing.OD, p8.y)
+	p10 = App.Vector(p9.x, 0)
+	p11 = App.Vector(p1.x, p10.y)
 
 	#TODO: load points into an array and play back 
 	sketch.addGeometry(Part.LineSegment(p1,p2))
@@ -68,6 +74,11 @@ if(mp.stator.IsInrunner == True):
 	sketch.addGeometry(Part.LineSegment(p3,p4))
 	sketch.addGeometry(Part.LineSegment(p4,p5))
 	sketch.addGeometry(Part.LineSegment(p5,p6))
+	sketch.addGeometry(Part.LineSegment(p6,p7))
+	sketch.addGeometry(Part.LineSegment(p7,p8))
+	sketch.addGeometry(Part.LineSegment(p8,p9))
+	sketch.addGeometry(Part.LineSegment(p9,p10))
+	sketch.addGeometry(Part.LineSegment(p10,p11))
 	
 
 doc.recompute()
