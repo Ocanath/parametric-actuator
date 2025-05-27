5e6254f9172ef1a615100a7e6ad73dd0aac2d018
ocanath <ocanath@gmail.com>
2025-05-26 18:24:31 -0700

    reverting

--- generate_actuator.py
+++ generate_actuator.py
@@ -1,15 +1,6 @@
 import FreeCAD as App
 import Part
 import Sketcher
-import os
-import sys
-
-# Add the current directory to Python path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-	sys.path.append(current_dir)
-
-from SketchHelperFunctions import create_constrained_line, join_lines, LineOrientation
 
 class MotorControlPCB:
 	def __init__(self):
@@ -22,7 +13,7 @@ class MotorControlPCB:
 
 
 class Bearing:
-	def __init__(self, OD, ID, Height):
+	def __init__(self, ID, OD, Height):
 		self.ID = ID
 		self.OD = OD
 		self.Height = Height
@@ -70,48 +61,59 @@ sketch = doc.addObject("Sketcher::SketchObject", "StatorHousingCrossSection")
 sketch.Placement = App.Placement(App.Vector(0,0,0), App.Rotation(App.Vector(1,0,0), 90))	# Rotate 90 degrees around X axis to get XZ plane
 
 if(mp.stator.IsInrunner == True):
-	origin = App.Vector(0,0,0)
+	p = []
+	
+	p.append(App.Vector(mp.bottomBearing.OD - mp.bottomBearing.OuterRaceClearance,0))
+	p.append(App.Vector(p[0].x,mp.BottomBearingSupportThickness))
+	innerWallRadius = mp.stator.StatorOD	#line to line
+	p.append(App.Vector(innerWallRadius-mp.stator.StatorODMountClearance, p[len(p)-1].y))
+	p.append(App.Vector(p[len(p)-1].x, p[len(p)-1].y + mp.stator.WireClearance)) #vertical
+	p.append(App.Vector(innerWallRadius, p[len(p)-1].y)) #horizontal
+	heightFromStatorBase  = mp.stator.StatorHeight + mp.topBearing.Height + mp.GearboxHeightClearance
+	p.append(App.Vector(p[len(p)-1].x, p[len(p)-1].y + heightFromStatorBase)) #vertical	#increase y to add clearance for the top bearing
+	outerWallRadius = innerWallRadius + mp.OuterWallThickness
+	p.append(App.Vector(outerWallRadius, p[len(p)-1].y))	#horizontal
+
+	"""
+	Create a seat for the pcb. Note: for an inrunner with top and bottom bearing, the rotor will rest on the bearing races and will be retained that way, 
+	so retaining rings should not be necessary
+	"""
+	distFromOriginToBase = mp.bottomBearing.Height + mp.mctl_pcb.BoardThickness + mp.mctl_pcb.EncoderAirgap + mp.mctl_pcb.EncoderHeight 
+	p.append(App.Vector(p[len(p)-1].x, p[0].y - distFromOriginToBase))
+	p.append(App.Vector(mp.mctl_pcb.BoardRadius, p[len(p)-1].y))
+	p.append(App.Vector(p[len(p)-1].x, p[len(p)-1].y + mp.mctl_pcb.BoardThickness))
+	p.append(App.Vector(p[len(p)-1].x-mp.mctl_pcb.BoardSeatClearance, p[len(p)-1].y))
+	p.append(App.Vector(p[len(p)-1].x, p[0].y - mp.bottomBearing.Height))
+	p.append(App.Vector(mp.bottomBearing.OD, p[len(p)-1].y))
+	p.append(App.Vector(p[len(p)-1].x, 0))
+	p.append(App.Vector(p[0].x, p[len(p)-1].y))
+
+
+
+	#TODO: load points into an array and play back 
 	lines = []
-	lines.append(create_constrained_line(sketch, 
-						App.Vector(mp.bottomBearing.OD - mp.bottomBearing.OuterRaceClearance,0),
-						mp.BottomBearingSupportThickness,
-						LineOrientation.VERTICAL))
+	for i in range(len(p)-1):
+		line = sketch.addGeometry(Part.LineSegment(p[i],p[i+1]))
+		lines.append(line)
+		if i > 0:  # For all lines after the first one
+			# Make end point of previous line coincident with start point of current line
+			sketch.addConstraint(Sketcher.Constraint('Coincident', lines[i-1], 2, lines[i], 1))
+		elif i == 0:  # For the first line
+			# Fix the start point at the origin using distance constraints
+			sketch.addConstraint(Sketcher.Constraint('DistanceX', lines[i], 1, p[0].x))  # Fix x at 0
+			sketch.addConstraint(Sketcher.Constraint('DistanceY', lines[i], 1, p[0].y))  # Fix y at 0
+		
+		# Check if line is horizontal (same y coordinates)
+		if abs(p[i].y - p[i+1].y) < 1e-6:
+			sketch.addConstraint(Sketcher.Constraint('Horizontal', i))
+			# Add distance constraint for horizontal lines
+			sketch.addConstraint(Sketcher.Constraint('Distance', i, abs(p[i+1].x - p[i].x)))
+		# Check if line is vertical (same x coordinates)
+		elif abs(p[i].x - p[i+1].x) < 1e-6:
+			sketch.addConstraint(Sketcher.Constraint('Vertical', i))
+			# Add distance constraint for vertical lines
+			sketch.addConstraint(Sketcher.Constraint('Distance', i, abs(p[i+1].y - p[i].y)))
 	
-
-	# p.append(App.Vector(mp.bottomBearing.OD - mp.bottomBearing.OuterRaceClearance,0))
-	# p.append(App.Vector(p[0].x,mp.BottomBearingSupportThickness))
-	lines.append(create_constrained_line(sketch, origin, mp.stator.StatorOD/2 - mp.stator.StatorODMountClearance, LineOrientation.HORIZONTAL))
-	join_lines(sketch, lines[0], lines[1])
-
-	
-	# innerWallRadius = mp.stator.StatorOD	#line to line
-	# p.append(App.Vector(innerWallRadius-mp.stator.StatorODMountClearance, p[len(p)-1].y))
-	# p.append(App.Vector(p[len(p)-1].x, p[len(p)-1].y + mp.stator.WireClearance)) #vertical
-
-	
-						 
-
-	# p.append(App.Vector(innerWallRadius, p[len(p)-1].y)) #horizontal
-	# heightFromStatorBase  = mp.stator.StatorHeight + mp.topBearing.Height + mp.GearboxHeightClearance
-	# p.append(App.Vector(p[len(p)-1].x, p[len(p)-1].y + heightFromStatorBase)) #vertical	#increase y to add clearance for the top bearing
-	# outerWallRadius = innerWallRadius + mp.OuterWallThickness
-	# p.append(App.Vector(outerWallRadius, p[len(p)-1].y))	#horizontal
-
-	# """
-	# Create a seat for the pcb. Note: for an inrunner with top and bottom bearing, the rotor will rest on the bearing races and will be retained that way, 
-	# so retaining rings should not be necessary
-	# """
-	# distFromOriginToBase = mp.bottomBearing.Height + mp.mctl_pcb.BoardThickness + mp.mctl_pcb.EncoderAirgap + mp.mctl_pcb.EncoderHeight 
-	# p.append(App.Vector(p[len(p)-1].x, p[0].y - distFromOriginToBase))
-	# p.append(App.Vector(mp.mctl_pcb.BoardRadius, p[len(p)-1].y))
-	# p.append(App.Vector(p[len(p)-1].x, p[len(p)-1].y + mp.mctl_pcb.BoardThickness))
-	# p.append(App.Vector(p[len(p)-1].x-mp.mctl_pcb.BoardSeatClearance, p[len(p)-1].y))
-	# p.append(App.Vector(p[len(p)-1].x, p[0].y - mp.bottomBearing.Height))
-	# p.append(App.Vector(mp.bottomBearing.OD, p[len(p)-1].y))
-	# p.append(App.Vector(p[len(p)-1].x, 0))
-	# p.append(App.Vector(p[0].x, p[len(p)-1].y))
-
-
 
 doc.recompute()
 
@@ -123,7 +125,5 @@ revolve.Base = (0, 0, 0)  # Origin point
 revolve.Angle = 360  # Full revolution
 
 doc.recompute()
-
-
 
 
